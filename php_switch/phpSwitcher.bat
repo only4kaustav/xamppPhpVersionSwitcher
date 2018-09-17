d9f4c20e4b76a67714dc4f408b65f60a77364d80
Kaustav Ghosh <kghosh8@its.jnj.com>
2018-09-17 19:30:43 +0530

    v3 version of Xampp PHP version Switcher

--- php_switch/phpSwitcher.bat
+++ php_switch/phpSwitcher.bat
@@ -5,12 +5,20 @@ IF NOT EXIST xampp_path.txt (
   exit
 )
 for /f "tokens=*" %%A in (xampp_path.txt) do (set xampp_path=%%A)
+IF "%xampp_path%" equ "" (
+  echo xampp directory path not provided in "xampp_path.txt"
+  pause >nul
+  exit
+) ELSE (
+  set "xampp_path=%xampp_path: =%"
+)
 IF NOT EXIST %xampp_path%apache_start.bat (
   echo Please specify full path of xampp directory in "xampp_path.txt"
   pause >nul
   exit
 )
-set /p phpversion=Which version you want to use 5.6 or 7.1 or 7.2 (5/71/72) :
+set /p phpversion=Which version you want to use 5.6 or 7.1 or 7.2 (5/71/72) : 
+set "phpversion=%phpversion: =%"
 set file_name=%cd%\php%phpversion%\httpd.conf
 IF EXIST %file_name% (
  xcopy %file_name% %xampp_path%apache\conf\httpd.conf /E /H /C /R /Q /Y
